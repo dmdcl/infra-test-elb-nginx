40fa968579fdf27207b561bdb4c33382f65b5038
dmdcl <diegogael13@hotmail.com>
2025-07-23 21:49:41 -0600

    refactor: changed the variable names

--- variables.tf
+++ variables.tf
@@ -1,47 +1,47 @@
-variable "awsRegion" {
-    description = "AWS region"
-    type = string
-    default = "us-east-1"
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
 
-variable "vpcCidr" {
-    description = "VPC CIDR block"
-    type = string
-    default = "10.0.0.0/16"
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
-variable "publicSubnets" {
-    description = "Public subnets CIDR"
-    type = list(string)
-    default = ["10.0.1.0/24", "10.0.2.0/24"]
+variable "public_subnets" {
+  description = "Public subnets CIDR"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-variable "privateSubnets" {
-    description = "Private subnets CIDR"
-    type = list(string)
-    default = ["10.0.3.0/24", "10.0.4.0/24"]
+variable "private_subnets" {
+  description = "Private subnets CIDR"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
-variable "appInstances" {
-    description = "App instance names"
-    type = list(string)
-    default = ["app1", "app2", "app3"]
+variable "app_instances" {
+  description = "App instance names"
+  type        = list(string)
+  default     = ["App1", "App2", "App3"]
 }
 
-variable "InstanceType" {
-    description = "EC2 instance type"
-    type = string
-    default = "t3.micro"
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
 }
 
-variable "KeyName"{
-    description = "SSH key pair name"
-    type = string
-    default = "myKeyPair"
+variable "key_name" {
+  description = "SSH key pair name"
+  type        = string
+  sensitive   = true
 }
 
-variable "myIp" {
-    description = "Public IP address for SSH access"
-    type = string
-    sensitive = true
+variable "my_ip" {
+  description = "Your public IP for SSH"
+  type        = string
+  sensitive   = true
 }
